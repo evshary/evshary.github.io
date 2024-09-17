97bc5da752f4cce070fb10caa077e376cdee577d
ChenYing Kuo <evshary@gmail.com>
2024-09-17 10:27:27 +0800

    Remove unnecessary files.
    Signed-off-by: ChenYing Kuo <evshary@gmail.com>

--- build_site.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Remove the old posts
-rm -rf source/_posts/*
-# Copy the new posts to the folder
-cp `find source/contents/ -name "*.md"` source/_posts/
-# Generate the site
-hexo clean && hexo generate
